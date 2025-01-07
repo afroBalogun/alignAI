0c39d450d4e3b6c3f9dff8a6ed0a3c624bead3a1
afroBalogun <148563288+afroBalogun@users.noreply.github.com>
2025-01-07 05:51:48 +0100

    mobile debug

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,48 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
 import { Outlet } from "react-router";
 
 export default function App(){
-    const [isLoading, setIsLoading] = useState(true);
+    // const [isLoading, setIsLoading] = useState(true);
 
-    useEffect(() => {
-        const hasLoadedBefore = localStorage.getItem('hasLoadedBefore');
-        if (hasLoadedBefore) {
-            setIsLoading(false);
-            return;
-        }
+    // useEffect(() => {
+    //     const hasLoadedBefore = localStorage.getItem('hasLoadedBefore');
+    //     if (hasLoadedBefore) {
+    //         setIsLoading(false);
+    //         return;
+    //     }
 
-        const minimumLoadingTime = 2000; 
-        const startTime = Date.now();
+    //     const minimumLoadingTime = 2000; 
+    //     const startTime = Date.now();
 
-        const handleLoad = () => {
-            const elapsedTime = Date.now() - startTime;
-            const remainingTime = minimumLoadingTime - elapsedTime;
+    //     const handleLoad = () => {
+    //         const elapsedTime = Date.now() - startTime;
+    //         const remainingTime = minimumLoadingTime - elapsedTime;
 
-            if (remainingTime > 0) {
-                setTimeout(() => {
-                    setIsLoading(false);
-                    localStorage.setItem('hasLoadedBefore', 'true');
-                }, remainingTime);
-            } else {
-                setIsLoading(false);
-                localStorage.setItem('hasLoadedBefore', 'true');
-            }
-        };
+    //         if (remainingTime > 0) {
+    //             setTimeout(() => {
+    //                 setIsLoading(false);
+    //                 localStorage.setItem('hasLoadedBefore', 'true');
+    //             }, remainingTime);
+    //         } else {
+    //             setIsLoading(false);
+    //             localStorage.setItem('hasLoadedBefore', 'true');
+    //         }
+    //     };
 
-        window.addEventListener('load', handleLoad);
+    //     window.addEventListener('load', handleLoad);
 
-        return () => {
-            window.removeEventListener('load', handleLoad);
-        };
-    }, []);
+    //     return () => {
+    //         window.removeEventListener('load', handleLoad);
+    //     };
+    // }, []);
 
-    if (isLoading) {
-        return( 
-            <div className="loading-screen">
-                <img src="images/alignAI-icon.png" alt="loading..." />
-            </div>);
-    }
+    // if (isLoading) {
+    //     return( 
+    //         <div className="loading-screen">
+    //             <img src="images/alignAI-icon.png" alt="loading..." />
+    //         </div>);
+    // }
 
     return(
         <div className="app">
