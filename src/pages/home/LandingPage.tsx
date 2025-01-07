4d35be51a1780b7fa9ef630d1a101483e94ece56
afroBalogun <148563288+afroBalogun@users.noreply.github.com>
2025-01-07 05:32:11 +0100

    test

--- src/pages/home/LandingPage.tsx
+++ src/pages/home/LandingPage.tsx
@@ -3,7 +3,7 @@ import Hero from "./Hero";
 
 export default function LandingPage(){ 
     return (
-        <div className="landing-page">
+        <div className="landing-page" style={{backgroundImage: `url("static/images/AI-home.jpg")`}}>
             <img src="static/images/left-intersect.png" alt="" className="left-intersect" />
             <img src="static/images/right-intersect.png" alt="" className="right-intersect" />
             <img src="static/images/l-eclipse.png" alt="eclipse" className="l-eclipse" />
