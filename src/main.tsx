78eabf77a0afefb47e2ac4c6412596a9fe25a377
afroBalogun <148563288+afroBalogun@users.noreply.github.com>
2025-01-07 06:20:54 +0100

    root color and index

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import './styles/index.css'
 import "./styles/home.css"
 import "./styles/about.css"
 import "./styles/pricing.css"
